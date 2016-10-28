8095f17ae04bc028402db4de773b0a8e5fb6f0c1
pellote <juancarlos@papinotas.com>
2016-10-28 16:10:37 -0300

    me demore pero lo logre

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,16 +1,45 @@
 class ProjectsController < ApplicationController
-  before_filter :set_project, only: [:show]
+  before_filter :set_project, only: [:show, :edit, :update]
+
+  def index
+  	@projects = Project.all
+  end
 
   def new
+  	@project = Project.new
   end
 
   def show
     
   end
 
+  def edit
+  	
+  end
+
+  def create
+  	@project = Project.new(project_params)
+
+  	respond_to do |format|
+  		if @project.save
+  			format.html { redirect_to @project , :notice => "Lo logre padre" }
+  		else
+  			format.html { redirect_to :new, :notice => "no lo logre pabre" }
+  		end
+  	end
+  end
+
+  def update
+  	
+  end
+
   private
   def set_project
     @project = Project.find(params[:id])
+  end
+
+  def project_params
+  	params.require(:project).permit(:name, :successful_deploy_url, :unsuccessful_deploy_url, :pending_deploy_url)
   end
 
 
