f991d4fd71a9fc5b6e6f15f9dcf20ce326535a97
Daniel Merrill Silva <danielmerrillsilva@Daniels-MacBook-Pro.local>
2017-01-04 18:03:15 -0300

    Added X-Frame header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def iframe_action
+    response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM http://dash-battle.herokuapp.com"
+    response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM http://localhost:3000"
+    response.headers["X-FRAME-OPTIONS"] = "ALLOW-FROM http://localhost:3001"
+  end
+
   def after_sign_in_path_for(resource)
     request.env['omniauth.origin'] || stored_location_for(resource) || projects_path
   end
